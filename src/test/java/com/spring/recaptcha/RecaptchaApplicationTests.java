b38088516e29bb453753c3b121389b03af8cf138
Igor Prilepov <sliceyounice@gmail.com>
2020-08-07 11:19:20 +0300

    spring project import

---
+++ src/test/java/com/spring/recaptcha/RecaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.recaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
