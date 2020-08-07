175bff374b829a26f4848fdbe6fe6f000bc55a9c
Igor Prilepov <sliceyounice@gmail.com>
2020-08-07 12:07:07 +0300

    added google response container class

---
+++ src/main/java/com/spring/recaptcha/payload/CaptchaResponse.java
@@ -0,0 +1,67 @@
+package com.spring.recaptcha.payload;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public class CaptchaResponse {
+
+    private boolean success;
+
+    @JsonProperty("challenge_ts")
+    private String challengeTs;
+
+    @JsonProperty("error-codes")
+    private ErrorCode[] errorCodes;
+
+    enum ErrorCode {
+        MissingSecret("missing-input-secret"),
+        InvalidSecret("invalid-input-secret"),
+        MissingResponse("missing-input-response"),
+        InvalidResponse("invalid-input-response"),
+        BadRequest("bad-request"),
+        TimeoutOrDuplicate("timeout-or-duplicate");
+
+        private final String value;
+
+        public String getValue() {
+            return this.value;
+        }
+
+        ErrorCode(String value) {
+            this.value = value;
+        }
+
+        @JsonCreator
+        public static ErrorCode forValue(String value) {
+            return Arrays.stream(ErrorCode.values())
+                    .filter(errorCode -> errorCode.getValue().equals(value))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Unknown enum value"));
+        }
+    }
+
+    @JsonIgnore
+    public boolean hasClientError() {
+        if (this.errorCodes == null)
+            return false;
+        for (ErrorCode error : this.errorCodes) {
+            switch (error) {
+                case MissingResponse:
+                case InvalidResponse:
+                case BadRequest:
+                case TimeoutOrDuplicate:
+                    return true;
+                default:
+                    break;
+            }
+        }
+        return false;
+    }
+}
