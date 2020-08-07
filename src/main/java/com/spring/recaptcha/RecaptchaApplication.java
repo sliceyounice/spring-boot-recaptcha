5e513dad539ada1913c5a38d9af2700570b232c2
Igor Prilepov <sliceyounice@gmail.com>
2020-08-07 12:56:03 +0300

    unirest config

--- src/main/java/com/spring/recaptcha/RecaptchaApplication.java
+++ src/main/java/com/spring/recaptcha/RecaptchaApplication.java
@@ -1,13 +1,22 @@
 package com.spring.recaptcha;
 
+import kong.unirest.JacksonObjectMapper;
+import kong.unirest.Unirest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RecaptchaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecaptchaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> Unirest.config().setObjectMapper(new JacksonObjectMapper());
 	}
 
 }
